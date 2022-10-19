f982139eb14e529f9738bfbc8bef5b8e22513393
Admin <Admin@TEB123011>
2022-10-19 11:27:45 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            bool b = true;
+            for(int i = 10; b == true; i = i + n)
+            {
+                Console.WriteLine(i);
+                switch (i)
+                {
+                    case 30:
+                        i = 200;
+                        n = -1;
+                        break;
+                    case 180:
+                        i = 1000;
+                        n = 50;
+                        break;
+                    case 1400:
+                        b = false;
+                        break;
+
+
+                }
+            }
+        }
+    }
+}
