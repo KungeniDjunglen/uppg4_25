192f47e68631064ceeae314ee555b515391adc79
KungeniDjunglen <111962627+KungeniDjunglen@users.noreply.github.com>
2022-10-19 12:12:22 +0200

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace program
 {
@@ -6,27 +6,43 @@ namespace program
     {
         static void Main(string[] args)
         {
-            int n = 1;
-            bool b = true;
-            for(int i = 10; b == true; i = i + n)
-            {
-                Console.WriteLine(i);
-                switch (i)
-                {
-                    case 30:
-                        i = 200;
-                        n = -1;
-                        break;
-                    case 180:
-                        i = 1000;
-                        n = 50;
-                        break;
-                    case 1400:
-                        b = false;
-                        break;
 
+            int n1 = 0;
+            int n2 = 0;
+            bool b = true;
+            while (b == true)
+            {
+                Console.WriteLine("Välj ett av förljande alternativ");
+                Console.WriteLine("1. Subtrahera ett tal med ett annat.");
+                Console.WriteLine("2. Mulitplicera.");
+                Console.WriteLine("3. Avlsuta program.");
+                int val = int.Parse(Console.ReadLine());
+
+                switch (val)
+                {
+                    case 1:
+                        Console.WriteLine("Skriv in de två talen:");
+                        n1 = int.Parse(Console.ReadLine());
+                        n2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine(n1 - n2);
+                        break;
+                    case 2:
+                        Console.WriteLine("Skriv in de två talen:");
+                        n1 = int.Parse(Console.ReadLine());
+                        n2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine(n1 * n2);
+                        break;
+                    case 3:
+                        b = false;
+                        Console.WriteLine("Du har avslutat programmet");
+                        break;
+                    default:
+                        Console.WriteLine("Du har valt fel nummer! ");
+                        break;
 
                 }
+
+
             }
         }
     }
